8a7a1eb0aa38433f34a1c67d44d4fd9aa30ca162
SoroushMomtahan <132217667+SoroushMomtahan@users.noreply.github.com>
2024-12-26 11:23:37 +0330

    Add View Model & View Repository

--- src/Shared/LearnTop.Shared.Application/Pagination/PaginationRequest.cs
+++ src/Shared/LearnTop.Shared.Application/Pagination/PaginationRequest.cs
@@ -1,9 +1,2 @@
 ﻿namespace LearnTop.Shared.Application.Pagination;
-
-
-/// <summary>
-/// ظرفی برای صفحه بندی درخواست
-/// </summary>
-/// <param name="PageIndex"></param>
-/// <param name="PageSize"></param>
 public record PaginationRequest(int PageIndex = 0, int PageSize = 10);
